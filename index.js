329a73a26ba5581d746f4b9daa179e63c486e87b
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-07-11 15:28:19 -0600

    classes variables including private variables in js set up

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here
+class Breakfast{
+    constructor(mfd, mdrnk)
+    {
+        this.food = mfd;
+        this.drink = mdrnk;
+    }
+}
+
+class Lunch{
+    constructor(sld, sp, mdrnk)
+    {
+        this.salad = sld;
+        this.soup = sp;
+        this.drink = mdrnk; 
+    }
+}
+
+class Dinner{
+    #dessert;
+    constructor(sld, sp, mnml, mdsrt)
+    {
+        this.salad = sld;
+        this.soup = sp;
+        this.entree = mnml;
+        this.#dessert = mdsrt;
+    }
+}
